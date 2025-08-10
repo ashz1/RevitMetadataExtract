5dee46b041376d7ecceb44696a59ef74e1bcc431
ashz1 <aashayzende@gmail.com>
2025-08-10 19:55:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -280,4 +280,4 @@ app.post('/extract', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
